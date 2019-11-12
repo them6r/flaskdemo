84b083078a5f1b6047f62c97efb17493519d1bec
them6r <matar.khal@gmil.com>
2019-11-12 13:36:57 +0800

    did posts

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+posts = [
+    {
+        'auther': 'Khamel Galafel',
+        'title': 'Blog post 1',
+        'content': 'fist post content',
+        'data_posted': 'April 23, 2019'
+    },
+    {
+        'auther': 'Samuel jakson',
+        'title': 'Blog post 2',
+        'content': 'second post content',
+        'data_posted': 'October 19, 2013'
+    }
+]
+
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 @app.route('/about')
 def about():
