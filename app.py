0ca2c9474cb3694814f18bcd7882488d2f7414a2
them6r <matar.khal@gmil.com>
2019-11-12 15:26:25 +0800

    added css and nav bar end of vid3

--- app.py
+++ app.py
@@ -1,29 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 posts = [
     {
-        'auther': 'Khamel Galafel',
-        'title': 'Blog post 1',
-        'content': 'fist post content',
-        'data_posted': 'April 23, 2019'
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
     },
     {
-        'auther': 'Samuel jakson',
-        'title': 'Blog post 2',
-        'content': 'second post content',
-        'data_posted': 'October 19, 2013'
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
     }
 ]
 
-@app.route('/')
-@app.route('/home')
+
+@app.route("/")
+@app.route("/home")
 def home():
     return render_template('home.html', posts=posts)
 
-@app.route('/about')
+
+@app.route("/about")
 def about():
     return render_template('about.html', title='About')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
